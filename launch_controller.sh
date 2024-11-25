7225a795aa3a3ed3d8311e1cc971bfaab13c66d8
elisethon <145268793+elisethon@users.noreply.github.com>
2024-11-25 09:45:05 +0100

    Update launch_controller.sh

--- launch_controller.sh
+++ launch_controller.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script to launch ROS 2 nodes on the Dev PC
+# Script to launch ROS 2 nodes for controller on the Dev PC
 
-# Start the joy_node for joystick control
+# Start the joy_node
 echo "Starting joy_node..."
 gnome-terminal -- bash -c "ros2 run joy joy_node; exec bash"
 
-# Launch teleop_twist_joy to convert joystick input to twist messages
+# Launch teleop_twist_joy 
 echo "Starting teleop_twist_joy..."
 gnome-terminal -- bash -c "ros2 launch teleop_twist_joy teleop-launch.py; exec bash"
 
